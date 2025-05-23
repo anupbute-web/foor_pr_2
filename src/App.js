434a39bffe17537b9f5814e004003516a3b2e0de
Anup_Bute <anupbute7517355998@gmail.com>
2025-05-23 23:16:55 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <div className='App'>
+
+   </div>
+    
   );
 }
 
 export default App;
+// document.getElementById("hamburger").addEventListener("click", function () {
+//             document.querySelector(".home_nav_nav").style.display = "none";
+//             document.querySelector(".open_nav").style.display = "block";
+//         });
